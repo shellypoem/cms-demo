6a9158dcdb7bd36e249137a445bcf98b29050f89
chenluqing <shellypoem@163.com>
2020-04-14 17:09:32 +0800

    1

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
+/*
+ * @Descripttion: 
+ * @Author: luqing Chen
+ * @Date: 2020-04-14 10:35:13
+ * @LastEditors: luqing Chen
+ * @LastEditTime: 2020-04-14 14:21:02
+ */
 import Vue from 'vue'
 import App from './App.vue'
-
+import 'normalize.css/normalize.css'
 Vue.config.productionTip = false
-
+import VuePreview from 'vue-preview';
+Vue.use(VuePreview);
 new Vue({
   render: h => h(App),
 }).$mount('#app')
---
+++ src/permission.js
@@ -0,0 +1,7 @@
+/*
+ * @Descripttion: 
+ * @Author: luqing Chen
+ * @Date: 2020-04-14 14:20:06
+ * @LastEditors: luqing Chen
+ * @LastEditTime: 2020-04-14 14:20:06
+ */
