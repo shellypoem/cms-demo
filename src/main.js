9d5006d15fcbceb4feb30e76b29316b0bf436690
chenluqing <shellypoem@163.com>
2020-04-14 19:36:37 +0800

    mainjs

--- src/main.js
+++ src/main.js
@@ -3,14 +3,23 @@
  * @Author: luqing Chen
  * @Date: 2020-04-14 10:35:13
  * @LastEditors: luqing Chen
- * @LastEditTime: 2020-04-14 14:21:02
+ * @LastEditTime: 2020-04-14 19:33:17
  */
 import Vue from 'vue'
 import App from './App.vue'
 import 'normalize.css/normalize.css'
 Vue.config.productionTip = false
 import VuePreview from 'vue-preview';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import store from './store'
+import router from './router'
+
+import '@/permission' // permission control
+Vue.use(ElementUI);
 Vue.use(VuePreview);
 new Vue({
+  store,
+  router,
   render: h => h(App),
 }).$mount('#app')
